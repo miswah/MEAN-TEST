f7551c8067af6ba53d94e8b2a4de0da458db5f64
obemyshinnyrod <miswah.md@gmail.com>
2022-08-13 11:34:45 +0530

    (feat): user list component done

--- client/src/app/user/user-list/user-list.component.ts
+++ client/src/app/user/user-list/user-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { catchError, EMPTY, Observable, tap } from 'rxjs';
+import { UserManagementService } from 'src/app/services/user-management.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
+  public userList$: Observable<any> = this.userService.getUserList().pipe(
+    catchError((err) => {
+      console.log(err);
+      return EMPTY;
+    })
+  );
 
-  constructor() { }
+  constructor(private userService: UserManagementService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
