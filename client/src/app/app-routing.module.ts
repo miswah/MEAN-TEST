31961578bab73b0df12cce8dfbc862097ca4535c
obemyshinnyrod <miswah.md@gmail.com>
2022-08-13 10:47:11 +0530

    (routing): set routing for each component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserAddComponent } from './user/user-add/user-add.component';
+import { UserDetailsComponent } from './user/user-details/user-details.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserUpdateComponent } from './user/user-update/user-update.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserListComponent },
+  { path: 'create', component: UserAddComponent },
+  { path: 'update', component: UserUpdateComponent },
+  { path: 'view/:id', component: UserDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserAddComponent } from './user/user-add/user-add.component';
+import { UserUpdateComponent } from './user/user-update/user-update.component';
+import { UserDetailsComponent } from './user/user-details/user-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserAddComponent,
+    UserUpdateComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
